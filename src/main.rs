0d527ac3fa434d03b8cc70a59b92f3d07b1717cd
Karol Zwolak <karolzwolak7@gmail.com>
2025-11-09 16:59:12 +0100

    actually init new memory functions

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@
 
 use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-use rdos::{hlt_loop, init, vga_println};
+use rdos::{
+    hlt_loop, init,
+    memory::{self, BootInfoFrameAllocator},
+    vga_println,
+};
+use x86_64::VirtAddr;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -23,8 +28,12 @@ fn panic(info: &PanicInfo) -> ! {
 
 entry_point!(kernel_main);
 
-pub fn kernel_main(_bootinfo: &'static BootInfo) -> ! {
+pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let _mapper = unsafe { memory::init(phys_mem_offset) };
+    let _frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     #[cfg(test)]
     test_main();
